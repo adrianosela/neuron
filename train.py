1da6ee86008d84cafc24fffcf0a6b92ff5cee1e7
Adriano <adriano.selaviles@gmail.com>
2020-05-14 13:37:16 -0700

    latest, cleaned up

--- train.py
+++ train.py
@@ -8,26 +8,25 @@ from keras.layers import Dense
 
 """
 prepare the data by segmenting it as follows:
-- x_train : 10 input features, 70% of full dataset
-- x_val   : 10 input features, 15% of full dataset
-- x_test  : 10 input features, 15% of full dataset
-- y_train : 1 label,           70% of full dataset
-- y_val   : 1 label,           15% of full dataset
-- y_test  : 1 label,           15% of full dataset
+- x_train : input features, (100 - test_partition) % of full dataset
+- x_val   : input features, (test_partition/2) % of full dataset
+- x_test  : input features, (test_partition/2) % of full dataset
+- y_train : label,          (100 - test_partition) % of full dataset
+- y_val   : label,          (test_partition/2) % of full dataset
+- y_test  : label,          (test_partition/2) % of full dataset
 """
-def prepare():
+def prepare(filename, features, test_partition):
     # load the dataset
-    df = pd.read_csv("./housepricedata.csv")
-    dset = df.values
-    # our x variable is columns 0-9 (all rows)
-    # our y variable is column 10   (all rows)
-    x, y = dset[:,0:10], dset[:,10]
-    # scale the input features (x) such that
-    # all values lie between 0 and 1 inclusive
-    mms = preprocessing.MinMaxScaler()
-    x_scaled = mms.fit_transform(x)
-    # split training, validation, and test data
-    x_train, x_val_and_test, y_train, y_val_and_test = train_test_split(x_scaled, y, test_size=0.3)
+    dset = pd.read_csv(filename).values
+    # our x variable is columns 0-features, all rows
+    x = dset[:,0:features]
+    # our y variable is the last column, all rows
+    y = dset[:,features]
+    # scale every input feature between 0 and 1 inclusive
+    x_scaled = preprocessing.MinMaxScaler().fit_transform(x)
+    # take (100 - test_partition)% of the data for training and test_partition% for validation and testing
+    x_train, x_val_and_test, y_train, y_val_and_test = train_test_split(x_scaled, y, test_size=test_partition)
+    # split valition and testing into separate partitions of equal size
     x_val, x_test, y_val, y_test = train_test_split(x_val_and_test, y_val_and_test, test_size=0.5)
     return x_train, x_val, x_test, y_train, y_val, y_test
 
@@ -46,11 +45,12 @@ def model():
         Dense(1, activation='sigmoid'),
     ])
     # compile model with stochastic gradient descent optimizer
+    # to determine the optimizing multipliers for the hidden layers
     model.compile(optimizer='sgd', loss='binary_crossentropy', metrics=['accuracy'])
     return model
 
 if __name__ == "__main__":
-    x_train, x_val, x_test, y_train, y_val, y_test = prepare()
+    x_train, x_val, x_test, y_train, y_val, y_test = prepare('./housepricedata.csv', 10, 0.3)
     model = model()
     model.fit(x_train, y_train, batch_size=32, epochs=100, validation_data=(x_val, y_val))
     print(model.evaluate(x_test, y_test)) # returns (loss, accuracy) tuple
